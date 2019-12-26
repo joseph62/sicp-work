aad6c3d77039936df5cf73fc93a00d4ccb8e494d
Sean Joseph <theseanjoseph@gmail.com>
2019-12-26 12:53:04 -0500

    completed 1.1.6

---
+++ 1/conditionals.lisp
@@ -0,0 +1,19 @@
+(define (abs x)
+  (cond ((> x 0) x)
+        ((= x 0) 0)
+        ((< x 0) (- x))))
+
+(define (abs2 x)
+  (cond ((< x 0) (- x))
+        (else x)))
+
+(define (abs3 x)
+  (if (< x 0)
+    (- x)
+    x))
+
+(define (>= x y)
+  (or (> x y) (= x y)))
+
+(define (>== x y)
+  (not (< x y)))
