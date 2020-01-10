d59d680b2e228c528d31784a14e3af0457229ff7
Sean Joseph <theseanjoseph@gmail.com>
2020-01-09 21:04:21 -0500

    fixing count-leaves

--- 2/2/hierarchical-structures.scm
+++ 2/2/hierarchical-structures.scm
@@ -1,21 +1,21 @@
 (define x (cons (list 1 2) (list 3 4)))
 ; ((1 2) 3 4)
 
-(length x)
+;(length x)
 ; 3
 
-(count-leaves x)
+;(count-leaves x)
 ; 4
 
-(length (list x x))
+;(length (list x x))
 ; 2
 
-(count-leaves (list x x))
+;(count-leaves (list x x))
 ; 8
 
 (define (count-leaves x)
   (cond ((null? x) 0)
-        ((pair? x) 1)
+        ((not (pair? x)) 1)
         (else (+ (count-leaves (car x))
                  (count-leaves (cdr x))))))
     
