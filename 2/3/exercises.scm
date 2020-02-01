9977d74fff36c5176f27076ab44ee41cb4d55bf8
Sean Joseph <theseanjoseph@gmail.com>
2020-02-01 10:15:24 -0500

    Completed up to exercise 2.64

--- 2/3/exercises.scm
+++ 2/3/exercises.scm
@@ -55,3 +55,69 @@
 ; elements and much less efficient for membership 
 ; dependent operations as the size of the set will grow 
 ; with every single addition rather than when the addition is unique
+
+; 61, 62 - In representing-sets.scm
+
+; 63)
+(define (tree->list-1 tree)
+  (if (null? tree)
+    ()
+    (append (tree->list-1 (left-branch tree))
+            (cons (entry tree)
+                  (tree->list-1 (right-branch tree))))))
+
+(define (tree->list-2 tree)
+  (define (copy-to-list tree result-list)
+    (if (null? tree)
+      result-list
+      (copy-to-list (left-branch tree)
+                     (cons (entry tree)
+                           (copy-to-list (right-branch tree)
+                                         result-list)))))
+  (copy-to-list tree '()))
+
+; a) Both of these implementations return a list sorted in ascending order
+; b) tree->list-1 and tree->list-2 use the same order of operations,
+; however the second procedure keeps a running total during execution which
+; should result in a lower space complexity for the operation
+
+
+; 64)
+(define (list->tree elements)
+  (car (partial-tree elements (length elements))))
+
+(define (partial-tree es n)
+  (if (= n 0)
+    (cons '() es)
+    (let ((left-size (quotient (- n 1) 2)))
+      (let ((left-result (partial-tree es left-size)))
+        (let ((left-tree (car left-result))
+              (non-left-es (cdr left-result))
+              (right-size (- n (+ left-size 1))))
+          (let ((this-entry (car non-left-es))
+                (right-result (partial-tree (cdr non-left-es)
+                                            right-size)))
+            (let ((right-tree (car right-result))
+                  (remaining-es (cdr right-result)))
+              (cons (make-tree this-entry left-tree right-tree)
+                    remaining-es))))))))
+; a)
+; partial-tree takes a list of elements and the number of elements to include
+; in the resulting tree. If the number to include is zero, return an empty 
+; value and the list of elements as the remaining. Then calculate the size of
+; what will become the left sub tree for the result by finding half of the 
+; current number of elements to include minus one for the current node rounded down.
+; Next, partial-tree calls itself with the list of elements and the number of elements
+; to include as the half rounded down to get the left sub tree, the remaining elements
+; which includes the current entry and the right sub tree elements, and the size of the
+; right sub tree which is defined as the total number of elements to include minus the
+; number of elements to include and the current node entry. Using the elements left over
+; after constructing the left sub tree, take the next element as the entry for the current
+; node and the rest plus the right sub tree size calculated to call parital-tree again
+; to calculate the right sub tree. Finally, return a pair where the left element is
+; a node of entry, left sub tree, and right sub tree equal to the values calculated and
+; the right element is the remaining nodes after calculating the node.
+
+; b)
+; The number of operations will increase linearly with the number of elements provided
+; because the operation must happen for every element provided
--- 2/3/representing-sets.scm
+++ 2/3/representing-sets.scm
@@ -1,3 +1,5 @@
+; Unordered List Set
+
 (define (element-of-set? x set)
   (cond ((null? set) #f)
         ((equal? x (car set)) #t)
@@ -8,9 +10,77 @@
     set
     (cons x set)))
 
-(define (intersection-sets s1 s2)
+(define (intersection-set s1 s2)
   (filter (lambda (x) (element-of-set? x s2)) s1))
 
 ; 59)
-(define (union-sets s1 s2)
+(define (union-set s1 s2)
   (fold-right adjoin-set s2 s1))
+
+; Ordered List Set
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (car set)) #t)
+        ((< x (car set)) #f)
+        (else (element-of-set? x (cdr set)))))
+
+(define (intersection-set s1 s2)
+  (if (or (null? s1) (null? s2))
+    ()
+    (let ((x1 (car s1))
+          (x2 (car s2)))
+      (cond ((= x1 x2)
+             (cons x1
+                   (intersection-set (cdr s1)
+                                     (cdr s2))))
+            ((< x1 x2)
+             (intersection-set (cdr s1) s2))
+            ((> x1 x2)
+             (intersection-set s1 (cdr s2)))))))
+
+; 61)
+(define (adjoin-set x set)
+  (cond ((null? set) (list x))
+        ((< x (car set)) (cons x set))
+        ((> x (car set)) (cons (car set) 
+                               (adjoin-set x (cdr set))))
+        (else set)))
+
+; 62)
+(define (union-set s1 s2)
+  (cond ((null? s1) s2)
+        ((null? s2) s1)
+        (else
+          (let ((x1 (car s1))
+                (x2 (car s2))
+                (r1 (cdr s1))
+                (r2 (cdr s2)))
+            (cond ((= x1 x2) (cons x1 (union-set r1 r2)))
+                  ((> x1 x2) (cons x2 (union-set s1 r2)))
+                  ((< x1 x2) (cons x1 (union-set r1 s2))))))))
+
+(define (entry tree) (car tree))
+(define (left-branch tree) (cadr tree))
+(define (right-branch tree) (caddr tree))
+(define (make-tree entry left right) (list entry left right))
+
+(define (element-of-set? x set)
+  (cond ((null? set) #f)
+        ((= x (entry set)) #t)
+        ((< x (entry set)) 
+         (element-of-set? x (left-branch set)))
+        ((> x (entry set))
+         (element-of-set? x (right-branch set)))))
+
+(define (adjoin-set x set)
+  (cond ((null? set) (make-tree x () ()))
+        ((= x (entry set)) set)
+        ((< x (entry set))
+         (make-tree (entry set)
+                    (adjoin-set x (left-branch set))
+                    (right-branch set)))
+        ((> x (entry set))
+         (make-tree (entry set)
+                    (left-branch set)
+                    (adjoin-set x (right-branch set))))))
