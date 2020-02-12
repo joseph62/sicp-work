97bf70ddec87689795d2d9b3ea3c21db4fec1664
Sean Joseph <theseanjoseph@gmail.com>
2020-02-12 13:00:34 -0500

    Completed section 2.4.2

--- 2/4/complex-numbers.scm
+++ 2/4/complex-numbers.scm
@@ -1,27 +1,111 @@
 (define (square n)
   (* n n))
 
+(define (make-from-real-imag-rectangular real imag)
+  (attach-tag 'rectangular (list real imag)))
+
 (define (make-from-real-imag real imag)
-  (list (sqrt (+ (square real)
-                 (square imag)))
-        (atan imag real)))
+  (make-from-real-imag-rectangular real imag))
+
+(define (make-from-mag-ang-polar mag ang)
+  (attach-tag 'polar (list mag ang)))
 
 (define (make-from-mag-ang mag ang)
-  (list mag ang))
+  (make-from-mag-ang-polar mag ang))
 
-(define (real-part number)
-  (* (magnitude number)
-     (cos (angle number))))
+(define (attach-tag type-tag content)
+  (cons type-tag content))
 
-(define (imag-part number)
-  (* (magnitude number)
-     (sin (angle number))))
+(define (type-tag datum)
+  (if (pair? datum)
+    (car datum)
+    (error "Bad tagged datum -- TYPE-TAG" datum)))
 
-(define (magnitude number)
+(define (contents number)
+  (if (pair? datum)
+    (cdr datum)
+    (error "Bad tagged datum -- TYPE-TAG" datum)))
+
+(define (real-part-rectangular number)
   (car number))
 
-(define (angle number)
+(define (real-part-polar number)
+  (* (magnitude-polar number)
+     (cos (angle-polar number))))
+
+(define (real-part number)
+  (cond ((rectangular? number) 
+         (real-part-rectangular (contents number)))
+        ((polar? number)
+         (real-part-polar (contents number)))
+        (else (error "Unkown type -- REAL-PART" number))))
+
+(define (imag-part-rectangular number)
   (cadr number))
+
+(define (imag-part-polar number)
+  (* (magnitude-polar number)
+     (sin (angle-polar number))))
+
+(define (imag-part number)
+  (cond ((rectangular? number)
+         (imag-part-rectangular (contents number)))
+        ((polar? number)
+         (imag-part-polar (contents number)))
+        (else (error "Unknown type -- IMAG-PART"))))
+(define (magnitude-polar number)
+  (car number))
+
+(define (magnitude-rectangular number)
+  (sqrt (+ (square (real-part-rectangular number))
+           (square (imag-part-rectangular number)))))
+
+(define (magnitude number)
+  (cond ((rectangular? number)
+         (magnitude-rectangular (contents number)))
+        ((polar? number)
+         (magnitude-polar (contents number)))
+        (else (error "Unknown type -- MAGNITUDE"))))
+
+(define (angle-polar number)
+  (cadr number))
+
+(define (angle-rectangular number)
+  (atan (imag-part-rectangular number)
+        (real-part-rectangular number)))
+
+(define (angle number)
+  (cond ((rectangular? number)
+         (angle-rectangular (contents number)))
+        ((polar? number)
+         (angle-polar (contents number)))
+        (else (error "Unknown type -- ANGLE"))))
+
+
+
+(define (imag-part number)
+  (if (rectangular? (contents number))
+    (caddr (contents number))
+    (* (magnitude (contents number))
+       (sin (angle (contents number))))))
+
+(define (magnitude number)
+  (if (polar? (contents number))
+    (cadr (contents number))
+    (sqrt (+ (square (real-part (contents number)))
+             (square (imag-part (contents number)))))))
+
+(define (angle number)
+  (if (polar? (contents number))
+    (caddr (contents number))
+    (atan (imag-part (contents number))
+          (real-part (contents number)))))
+
+(define (rectangular? number)
+  (equal? (type-tag number) 'rectangular))
+
+(define (polar? number)
+  (equal? (type-tag number) 'polar))
 
 (define (add-complex z1 z2)
   (make-from-real-imag (+ (real-part z1) (real-part z2))
