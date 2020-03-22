4a8bd53eebaf8fe240ba3a76160faf38f6dcd3a3
Sean Joseph <theseanjoseph@gmail.com>
2020-03-21 21:56:01 -0400

    Completed section 3.2

--- 3/2/exercises.scm
+++ 3/2/exercises.scm
@@ -205,3 +205,107 @@
 ; right: 120
 
 ; 720
+
+; 10)
+
+(define (make-withdraw initial-amount)
+  (let ((balance initial-amount))
+	(lambda (amount)
+	  (if (>= balance amount)
+		  (begin (set! balance (- balance amount))
+				 balance)
+		  "Insufficient funds!"))))
+(define W1 (make-withdraw 100))
+(W1 50)
+(define W2 (make-withdraw 100))
+
+; Global:
+; make-withdraw
+; W1 -> E2
+; W2 -> E5
+
+; E1 -> Global
+; initial-amount: 100
+
+; E2 -> E1
+; balance: 100
+
+; E3 -> E2
+; amount: 50
+
+; E2 -> E1
+; balance: 50 
+
+; E4 -> Global
+; initial-amount: 100
+
+; E5 -> E4
+; balance: 100
+
+; Even though W1 and W2 are created with the same initial balance, they point to
+; two entirely separate envrionments.
+
+; 11)
+
+(define (make-account balance)
+  (define (withdraw amount)
+	(if (>= balance amount)
+		(begin (set! balance (- balance amount))
+			   balance)
+		"Insufficient funds!"))
+  (define (deposit amount)
+	(set! balance (+ balance amount))
+	balance)
+  (define (dispatch m)
+	(cond ((eq? m 'withdraw) withdraw)
+		  ((eq? m 'deposit) deposit)
+		  (else (error "Unknown request -- MAKE-ACCOUNT"
+					   m))))
+  dispatch)
+
+(define (deposit account amount)
+  ((account 'deposit) amount))
+
+(define (withdraw account amount)
+  ((account 'withdraw) amount))
+
+(define acc (make-account 50)) 
+(deposit acc 40)
+; 90
+(withdraw acc 60)
+; 30
+
+; Global
+; make-account
+; deposit
+; withdraw
+
+; E1 -> Global (make-account 50)
+; balance: 50
+; withdraw
+; deposit
+; dispatch
+
+; Global
+; acc: dispatch -> E1
+
+; E2 -> Global (deposit acc 40)
+
+; E3 -> E1 (acc 'deposit)
+
+; E4 -> E1 (deposit 40)
+
+; E1
+; balance: 90
+
+; E5 -> Global (withdraw acc 60)
+
+; E6 -> E1 (acc 'withdraw)
+
+; E7 -> E1 (withdraw 60)
+
+; E1
+; balance: 30
+
+; If another account was created using make-account, the resulting account
+; would use an entirely new envrionment to store it's balance, withdraw, deposit, and dispatch
