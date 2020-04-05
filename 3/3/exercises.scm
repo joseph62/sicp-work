5a375db08e55352e4a5858af56e0ccbe931b01ca
Sean Joseph <theseanjoseph@gmail.com>
2020-04-05 16:53:19 -0400

    Completed Exercise 3.24

--- 3/3/exercises.scm
+++ 3/3/exercises.scm
@@ -351,3 +351,40 @@ w
 		  (set-rear-ptr! d ())
 		  (set-next-ptr! d ())
 		  d)))
+
+; 24)
+(define (make-table same-key?)
+  (let ((local-table (list '*table*)))
+
+	(define (records table)
+	  (cdr table))
+
+	(define (assoc key records)
+	  (cond ((null? records) #f)
+			((same-key? key (caar records)) (car records))
+			(else (assoc key (cdr records)))))
+
+	(define (lookup key)
+	  (let ((record (assoc key (records local-table))))
+		(if record
+		  (cdr record)
+		  #f)))
+
+	(define (insert! key value)
+	  (let ((record (assoc key (records local-table))))
+		(if record
+		  (set-cdr! record value)
+		  (set-cdr! local-table (cons (cons key value)
+									  (records local-table))))))
+
+	(define (dispatch m)
+	  (cond ((eq? m 'lookup) lookup)
+			((eq? m 'insert!) insert!)
+			(else (error "Unkown command -- TABLE" m))))
+	dispatch))
+
+(define (lookup key table)
+  ((table 'lookup) key))
+
+(define (insert! key value table)
+  ((table 'insert!) key value))
