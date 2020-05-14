f19330967233f4bbe6399643837a9e52725b0b0c
Sean Joseph <theseanjoseph@gmail.com>
2020-05-14 12:49:22 -0400

    Completed Exercise 3.63

---
+++ 3/5/exercises/63.scm
@@ -0,0 +1,10 @@
+(define (sqrt-stream x)
+  (cons-stream 1.0
+               (stream-map (lambda (guess)
+                             (sqrt-improve guess x))
+                           (sqrt-stream x))))
+
+; This implementation is less efficient that the version
+; that binds the stream to guesses instead of recursing the procedure.
+; Recursing creates a new stream without the memoization.
+; If the memoization optimization was removed the two implementations would be equivalent.
