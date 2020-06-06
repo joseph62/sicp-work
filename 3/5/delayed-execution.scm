0ce9a405785fa0c690981c0bef46e6d2f5f21d73
Sean Joseph <theseanjoseph@gmail.com>
2020-06-06 11:35:33 -0400

    Completed Exercise 3.77

---
+++ 3/5/delayed-execution.scm
@@ -0,0 +1,20 @@
+(load "stream-paradigm.scm")
+
+;(define (solve f y0 dt)
+;  (define y (integral dy y0 dt))
+;  (define dy (stream-map f y))
+;  y)
+; Not possible without delayed evaluation of dy
+
+(define (integral delayed-integrand initial-value dt)
+  (define int
+    (cons-stream initial-value
+                 (let ((integrand (force delayed-integrand)))
+                   (add-streams (scale-stream integrand dt)
+                                int))))
+  int)
+
+(define (solve f y0 dt)
+  (define y (integral (delay dy) y0 dt))
+  (define dy (stream-map f y))
+  y)
