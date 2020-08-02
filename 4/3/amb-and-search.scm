c53cc27a93e2a33722de8ba3d6ebdb059103c6bd
Sean Joseph <theseanjoseph@gmail.com>
2020-08-01 18:34:15 -0700

    Started section 4.3

---
+++ 4/3/amb-and-search.scm
@@ -0,0 +1,18 @@
+(define (amb . possibilities)
+  (if (null? possibilities)
+      ()
+      (car possibilites))) ; pretend this returns some possibility instead of just the first
+
+(list (amb 1 2 3) (amb 'a 'b))
+; Possible values:
+; (1 a) (1 b) (2 a) (2 b) (3 a) (3 b)
+
+(define (require p)
+  (if (not p) (amb)))
+
+(define (an-element-of items)
+  (require (not (null? items)))
+  (amb (car items) (an-element-of (cdr items))))
+
+(define (an-integer-starting-from n)
+  (amb n (an-integer-starting-from (+ n 1))))
